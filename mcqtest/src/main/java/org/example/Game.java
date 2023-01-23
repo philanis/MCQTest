8f7b6b81306e11f899906322341ea4b49fe7965a
Nhlakanipho Shusha <shushanhlakanipho@gmail.com>
2023-01-23 09:35:23 +0200

    Add files via upload

---
+++ mcqtest/src/main/java/org/example/Game.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.*;
+
+public class Game {
+    private ArrayList<Question> questionSet;
+
+    public Game(){
+        questionSet = new ArrayList<Question>();
+        String q = "Where does the sun rise? ";
+        String[] a = {"East", "South", "West", "North"};
+        questionSet.add(new Question(q, a, "East"));
+        q = "Who invented the telephone?";
+        a = new String[]{"Thomas Edison", "Michael Faraday", "James Watt", "Alexander Bell"};
+        questionSet.add(new Question(q, a, "Alexander Bell"));
+        q = "What is the capital of South Africa?";
+        a = new String[]{"Durban", "Johannesburg", "Cape Town", "Pretoria"};
+        questionSet.add(new Question(q, a, "Pretoria"));
+        Collections.shuffle(questionSet, new Random());
+    }
+    public void start(){
+        Scanner scan = new Scanner(System.in);
+        int numCorrect = 0;
+        for(int question = 0; question <questionSet.size(); question++){
+            System.out.println(questionSet.get(question).getQuestion());
+            int numChoices = questionSet.get(question).getChoices().size();
+            for(int choice = 0; choice < numChoices; choice++){
+                System.out.println((choice + 1) + ": " + questionSet.get(question).getChoices().get(choice));
+            }
+            int playerAnswer = scan.nextInt();
+            ArrayList<String> choiceSet = questionSet.get(question).getChoices();
+            String correctAnswer = questionSet.get(question).getAnswer();
+            int correctAnswerIndex = choiceSet.indexOf(correctAnswer);
+            if(playerAnswer == correctAnswerIndex + 1){
+                numCorrect++;
+            }
+        }
+        scan.close();
+        System.out.println("You got " + numCorrect + " correct answer(s)");
+    }
+}
